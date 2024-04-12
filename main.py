0b5a5f48d6d48ca86a613df20c258639d1030309
Minh-Duc Bui <minhduc2k31905@gmail.com>
2024-04-13 04:26:25 +0700

    feat: prepare base model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestion
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModel
 
 
 try:
@@ -15,4 +16,15 @@ try:
     data_ingestion.extract_zip_file()
 except Exception as e:
     logger.error(e)
+    raise e
+
+
+
+try:
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+except Exception as e:
     raise e
